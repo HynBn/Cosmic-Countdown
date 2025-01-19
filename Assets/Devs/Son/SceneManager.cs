51caf1b4c557f3e53989f63400961b987b72ae8a
SonXH <136507369+SonXH@users.noreply.github.com>
2025-01-19 23:14:57 +0100

    add a loadPreviousMap function for the endscreen

--- Assets/Devs/Son/SceneManager.cs
+++ Assets/Devs/Son/SceneManager.cs
@@ -6,6 +6,8 @@ public class MySceneManager : MonoBehaviour
 
     public static MySceneManager Instance;
 
+    private int lastMap = 3;
+
     private void Awake()
     {
 
@@ -31,18 +33,31 @@ public class MySceneManager : MonoBehaviour
     public void LoadMap1()
     {
         SceneManager.LoadScene(1);
+        lastMap = 1;
     }
 
     // Load Map 2 scene
     public void LoadMap2()
     {
         SceneManager.LoadScene(2);
+        lastMap = 2;
+    }
+
+    public void LoadMap3()
+    {
+        SceneManager.LoadScene(5);
+        lastMap = 5;
     }
 
     // Load the Map Select scene
     public void LoadMapSelect()
     {
         SceneManager.LoadScene(3);
+    }
+
+    public void LoadPreviousMap()
+    {
+        SceneManager.LoadScene(lastMap);
     }
 
     // Load the End Screen scene
