118a8b3e9471d91dd23689c05d1e7cddcdd91a6d
HynB <135057697+HynBn@users.noreply.github.com>
2025-01-23 01:21:53 +0100

    Reapply "Add Settings Screen + cleaned up UI"
    This reverts commit 3bbe631ae3e88899faa4634fa9f7e2d1ccc838b2.

--- Assets/Devs/Son/GameManager.cs
+++ Assets/Devs/Son/GameManager.cs
@@ -127,7 +127,16 @@ public class GameManager : MonoBehaviour, IGameContext
 
         yield return new WaitForSeconds(endRoundDelay);
 
-        UpdateLives();
+        //UpdateLives();
+
+        if(winner == player1 && loser == player2)
+        {
+            Player2Lives--;
+        }
+        else
+        {
+            Player1Lives--;
+        }
 
         if (Player1Lives == 0 || Player2Lives == 0)
         {
