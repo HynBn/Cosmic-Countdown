0d151422577eb8193694c35d14aac6e52c80cd99
Hamzeh <hamzehx2@outlook.com>
2025-01-25 02:23:09 +0100

    Fxed issue with sound sliders and animation at end screen

--- Assets/Devs/Son/EndScreenManager.cs
+++ Assets/Devs/Son/EndScreenManager.cs
@@ -3,26 +3,27 @@ using TMPro;
 
 public class EndScreenManager : MonoBehaviour
 {
-
     [SerializeField]
     private TextMeshProUGUI winnerText;
-
     [SerializeField]
     private GameObject player1;
     [SerializeField]
     private GameObject player2;
-
     private Vector3 spawnpoint;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         spawnpoint = GameObject.Find("winnerSpawner").transform.position;
-
         var winner = PlayerPrefs.GetString("Winner") == "Player1" ? player1 : player2;
         winnerText.SetText(winner.name.ToString());
-
-
         winner.transform.position = spawnpoint;
 
+        // Get and reset the animator directly
+        Animator animator = winner.GetComponent<Animator>();
+        if (animator != null)
+        {
+            animator.SetBool("IsMoving", false);
+            animator.SetBool("IsJumping", false);
+        }
     }
 }
